e3ab5f936763c8cca87495147b98ee6637c462fd
Dmitry K <dimka.rs@gmail.com>
2018-05-28 23:53:45 +0300

    added i2c lcd 16x2

--- mixer.ino
+++ mixer.ino
@@ -1,3 +1,8 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
 //Relays. HIGH is ON
 #define MIXER 4    //mixer motor
 #define COOL_ON  5 //open cooling valve
@@ -16,15 +21,36 @@ void setup() {
     //pinMode(SMTH_EL, OUTPUT);
 
     pinMode(BUTTON, INPUT_PULLUP);
+
+    lcd.init();                      // Инициализация дисплея  
+    lcd.backlight();                 // Подключение подсветки
+    lcd.setCursor(0,0);              // Установка курсора в начало первой строки
+    lcd.print("TEMP:xx  STEP:xx");       // Набор текста на первой строке
+    lcd.setCursor(0,1);              // Установка курсора в начало второй строки
+    lcd.print("mixer     cooler");  
+    
     Serial.println("Main loop");
+}
+
+void SetMixer(int state)
+{
+  if(state==1) {
+    digitalWrite(MIXER, LOW);
+    lcd.setCursor(0,1);
+    lcd.print("mixer");
+  } else if (state==0){
+    digitalWrite(MIXER, HIGH);
+    lcd.setCursor(0,1);
+    lcd.print("MIXER");  
+  }
 }
 
 void loop() {
   int btn=digitalRead(0);
   if(btn == 1) {
-    digitalWrite(MIXER, LOW);  
+    SetMixer(1);  
   } else {
-    digitalWrite(MIXER, HIGH);
+    SetMixer(0);
   }
   Serial.println(btn);
   delay(1000);
