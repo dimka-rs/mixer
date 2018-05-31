eca86030d234e088373bb928ba4446486122f555
Dmitry K <dimka.rs@gmail.com>
2018-05-31 22:22:30 +0300

    draft steps 4-7

--- mixer.ino
+++ mixer.ino
@@ -2,8 +2,11 @@
 #include <LiquidCrystal_I2C.h>
 
 //constants
-#define TIME1 10 //time in sec to mix, step 1
+#define TIME1 31 //time in sec to mix, step 1
 #define TEMP2 40 //temp to cool to, step 2
+#define TEMP4 34 //temp to cool to, step 4
+#define TIME5 100 //time in sec to mix, step 5
+#define TEMP6 28 //temp to cool to, step 6
 
 /////////////////////////////////////////////
 // do not touch text below!
@@ -29,6 +32,17 @@ volatile int buzz = 0;
 volatile int cntdown = 0;
 
 //functions
+void SetBuzzer(int state)
+{
+  if(state==1){
+    digitalWrite(BUZZER, HIGH);
+    buzz=1;
+  } else if(state==0){
+    digitalWrite(BUZZER, LOW);
+    buzz=0;
+  }
+}
+
 void SetMixer(int state)
 {
   if(state==1) {
@@ -70,25 +84,41 @@ void UpdateDisplay()
 {
   ReadTemp();
   lcd.setCursor(0,0);
-  lcd.print("TEMP:");
+  lcd.print("T:");
+  if(temp < 10) lcd.print("0");
   lcd.print(temp);
 
-  lcd.setCursor(9,0);
-  lcd.print("STEP:");
+  lcd.setCursor(6,0);
+  lcd.print("C:");
+  if(cntdown < 100) lcd.print("0");
+  if(cntdown < 10)  lcd.print("0");
+   
+  lcd.print(cntdown);
+
+  lcd.setCursor(12,0);
+  lcd.print("S:");
+  if(step < 10) lcd.print("0");
   lcd.print(step);
   
   lcd.setCursor(0,1);
   if(mix==1) {
-    lcd.print("MIXER");
+    lcd.print("MIX");
   } else {
-    lcd.print("mixer");
+    lcd.print("mix");
   }
 
-  lcd.setCursor(10,1);
-  if(cool==1) {
-    lcd.print("COOLER");
+  lcd.setCursor(6,1);
+  if(buzz==1) {
+    lcd.print("BUZZ");
   } else {
-    lcd.print("cooler");
+    lcd.print("buzz");
+  }
+
+  lcd.setCursor(12,1);
+  if(cool==1) {
+    lcd.print("COOL");
+  } else {
+    lcd.print("cool");
   }
 }
 
@@ -146,6 +176,15 @@ void loop() {
   //step 0
   //just wait for start button
   SetStep(0);
+  SetBuzzer(0);
+  SetMixer(0);
+  SetCooler(0);
+  //wait for release
+  while(ReadBtn(BTN_START) == 1) {
+    UpdateDisplay();
+    delay(1000);
+  }
+  //wait for press
   while(ReadBtn(BTN_START) == 0) {
     UpdateDisplay();
     delay(1000);
@@ -173,9 +212,63 @@ void loop() {
   //step 3
   //wait for button
   SetStep(3);
-   while(ReadBtn(BTN_START) == 0) {
+  SetBuzzer(1);
+  //wait for release
+  while(ReadBtn(BTN_START) == 0) {
     UpdateDisplay();
     delay(1000);
   }
-  
+  //wait for press
+  while(ReadBtn(BTN_START) == 1) {
+    UpdateDisplay();
+    delay(1000);
+  }
+  SetBuzzer(0);
+  //DEBUG
+  temp=80;
+
+  //step 4
+  //cool until TEMP4
+  SetStep(4);
+  SetCooler(1);
+  while(temp > TEMP4) {
+    UpdateDisplay(); //refreshes temp
+    delay(1000);
+  }
+
+  //step 5
+  //mix for TIME5
+  SetStep(5);
+  SetMixer(1);
+  cntdown = TIME5;
+  while(DoCountdown()) {
+    UpdateDisplay();
+    // no delay here - DoCountdown does it
+  }
+
+  //step 6
+  //cooldown 1 deg/sec down to TEMP6
+  //TODO control cooldown speed
+  SetStep(6);
+  SetCooler(1);
+  while(temp > TEMP6) {
+    UpdateDisplay(); //refreshes temp
+    delay(1000);
+  }
+
+  //step 7
+  //buzz until button pressed
+  SetStep(7);
+  SetBuzzer(1);
+  //wait for release
+  while(ReadBtn(BTN_START) == 1) {
+    UpdateDisplay();
+    delay(1000);
+  }
+  //wait for press
+  while(ReadBtn(BTN_START) == 0) {
+    UpdateDisplay();
+    delay(1000);
+  }
+  SetBuzzer(0);
 }
